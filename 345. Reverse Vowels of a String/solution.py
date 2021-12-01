9727a46156adce32c1f87deedd7c14ea6825064d
Gul Jamal Zim <guljamalzim@gmail.com>
2021-12-01 16:53:48 +0600

    Solve problem #345

--- 345. Reverse Vowels of a String/solution.py
+++ 345. Reverse Vowels of a String/solution.py
@@ -1,21 +1,45 @@
 import random
 
 class Solution:
-    def reverse(self, x: int) -> int:
-        rev = 0
-        mp = -1 if x < 0 else 1
-        x *= mp
-        
-        while x:
-            rev = (rev * 10) + (x % 10)
-            x //= 10
+    def reverseVowels_slower(self, s: str) -> str:
+        vowels = { v: True for v in 'aeiouAEIOU' }
+        i, j = 0, len(s)-1
+        chars = list(s)
 
-        rev *= mp
-        return 0 if rev < -2**31 or rev >= 2**31 else rev
+        while i < j:
+            if chars[i] not in vowels:
+                i += 1
+
+            if chars[j] not in vowels:
+                j -= 1
+            
+            if i >= 0 and chars[i] in vowels and \
+               j < len(chars) and chars[j] in vowels:
+                chars[i], chars[j] = chars[j], chars[i]
+                i += 1
+                j -= 1
+
+        return ''.join(chars)
+
+    def reverseVowels(self, s: str) -> str:
+        s = list(s)
+        vowels = set('aeiouAEIOU')
+        l, r = 0, len(s)-1
+        
+        while l < r:
+            while l < r and s[l] not in vowels: l += 1
+            while l < r and s[r] not in vowels: r -= 1
+
+            if l >= r: break
+            
+            s[l], s[r] = s[r], s[l]
+            l, r = l+1, r-1
+
+        return ''.join(s)
+
 
 sol = Solution()
-print(sol.reverse(-8463847412))
-print(sol.reverse(7463847412))
-
+print(sol.reverseVowels('leetcode'))
+        
 
 
