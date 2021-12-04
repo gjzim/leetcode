9b3f3c8f5bdd46a085a7d455f4c1ca8a8a3d861f
Gul Jamal Zim <guljamalzim@gmail.com>
2021-12-04 18:30:15 +0600

    Implement O(m+n) solution for problem #1351

--- 1351. Count Negative Numbers in a Sorted Matrix/solution.py
+++ 1351. Count Negative Numbers in a Sorted Matrix/solution.py
@@ -2,7 +2,7 @@ import random
 from typing import List
 
 class Solution:
-    def countNegatives(self, grid: List[List[int]]) -> int:
+    def countNegatives_BF(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
         count = 0
         
@@ -13,6 +13,21 @@ class Solution:
                     break
                 
         return count
+
+    def countNegatives(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        row, col, count = m-1, 0, 0
+
+        while row >= 0 and col < n:
+            if grid[row][col] < 0:
+                row -= 1
+                count += (n-col)
+            else:
+                col += 1
+                
+        return count
+
+
 
 
 
