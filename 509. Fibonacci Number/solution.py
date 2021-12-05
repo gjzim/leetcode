6887f6c80b172409dbab4a63c0dafea090b510e6
Gul Jamal Zim <guljamalzim@gmail.com>
2021-12-05 16:44:48 +0600

    Solve problem #509

---
+++ 509. Fibonacci Number/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        if n < 2: return n
+
+        a, b = 0, 1
+        for _ in range(1, n):
+            a, b = b, (a+b)
+
+        return b
+
+sol = Solution()
+for n in range(30):
+    print(n, sol.fib(n))
+
+            
+        
