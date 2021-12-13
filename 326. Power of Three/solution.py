5600ffda440d01eef4554f9d06e9fb614a152f37
Gul Jamal Zim <guljamalzim@gmail.com>
2021-12-13 17:08:22 +0600

    Add problem #326 one line solution

--- 326. Power of Three/solution.py
+++ 326. Power of Three/solution.py
@@ -1,7 +1,10 @@
 class Solution:
-    def isPowerOfThree(self, n: int) -> bool:
+    def isPowerOfThree_loop(self, n: int) -> bool:
         while n > 1 and n % 3 == 0:
             n //= 3
             
         return n == 1
+
+    def isPowerOfThree(self, n: int) -> bool:
+        return n > 0 and (3**20) % n == 0
 
