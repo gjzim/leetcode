5c358bccced40ff4d56de7826866af73ca73565f
Gul Jamal Zim <guljamalzim@gmail.com>
2022-01-19 17:16:50 +0600

    Add nlogn solution to problem #209

--- 209. Minimum Size Subarray Sum/solution.py
+++ 209. Minimum Size Subarray Sum/solution.py
@@ -1,7 +1,8 @@
+import random
 from typing import List
 
 class Solution:
-    def minSubArrayLen(self, target: int, nums: List[int]) -> int:       
+    def minSubArrayLen_sw(self, target: int, nums: List[int]) -> int:       
         start = cursum = 0
         result = len(nums) + 1
 
@@ -14,4 +15,28 @@ class Solution:
                 start += 1
 
         return result % (len(nums) + 1)
-        
+
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        sums = [0] * (len(nums) + 1)
+        for i in range(1, len(nums) + 1):
+            sums[i] = sums[i - 1] + nums[i - 1]
+       
+        result = len(sums)
+        for start in range(len(sums)):
+            pos = self.binarySearch(sums, start, len(sums)-1, sums[start] + target)
+            if pos < len(sums):
+                result = min(result, pos - start)
+                
+        return result % len(sums)
+
+    def binarySearch(self, nums: List[int], start: int, end: int, target: int) -> int:
+        while start <= end:
+            mid = start + ((end-start)//2)
+            
+            if nums[mid] < target:
+                start = mid + 1
+            else:
+                end = mid - 1
+            
+        return start
+
