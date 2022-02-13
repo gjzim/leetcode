ffc7219c7f2afbca72b3f339163db1416827e274
Gul Jamal Zim <guljamalzim@gmail.com>
2022-02-13 19:34:57 +0600

    Solve problem #645 linear solution

--- 645. Set Mismatch/solution.py
+++ 645. Set Mismatch/solution.py
@@ -13,3 +13,16 @@ class Solution:
                 dup = i
 
         return [dup, missing]
+
+    def findErrorNums_linear(self, nums: List[int]) -> List[int]:
+        du, mis, l = 0, 0, len(nums)
+        for i in nums:
+            nums[i % l - 1] += l
+        
+        for i, v in enumerate(nums): 
+            if v <= l:
+                mis = i + 1
+            elif v > 2 * l:
+                du = i + 1
+            if du and mis:
+                return [du, mis]
