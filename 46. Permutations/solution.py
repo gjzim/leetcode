ddd258d0274aff819888b2235472923fa58835b3
Gul Jamal Zim <guljamalzim@gmail.com>
2022-05-06 18:07:08 +0600

    Add alternate solutions to problem #46

--- 46. Permutations/solution.py
+++ 46. Permutations/solution.py
@@ -1,18 +1,45 @@
 from typing import List
 
 class Solution:
-    def permute(self, nums: List[int]) -> List[List[int]]:
+    def permute_alt(self, nums: List[int]) -> List[List[int]]:
         if len(nums) == 1: return [nums]
 
-        out = []
-        for i, n in enumerate(nums):                        
+        result = []
+        for i, num in enumerate(nums):                        
             perms = self.permute(nums[:i] + nums[i+1:])
             
             for perm in perms:
-                perm.insert(0, n)
+                result.append([num] + perm)
 
-            out.extend(perms)
+        return result
 
-        return out
+    def permute_alt1(self, nums: List[int]) -> List[List[int]]:        
+        def generate(index, curPerm):
+            if index == len(nums):
+                result.append(curPerm)
+            else:
+                for i in range(len(curPerm) + 1):
+                    newPerm = curPerm[:]
+                    newPerm.insert(i, nums[index])
+                    generate(index + 1, newPerm)
+            
+        result = []
+        generate(0, [])        
+        return result
 
+    def permute(self, nums: List[int]) -> List[List[int]]:        
+        def generate(curPerm):
+            if len(curPerm) == len(nums):
+                result.append(curPerm[:])
+            else:
+                for i in range(len(nums)):
+                    if nums[i] in curPerm: continue
+                    
+                    curPerm.append(nums[i])
+                    generate(curPerm)
+                    curPerm.pop()
+            
+        result = []
+        generate([])        
+        return result
         
