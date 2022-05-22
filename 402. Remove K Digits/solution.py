9362300ba1ce20947d62d79c35c45934d524d384
Gul Jamal Zim <guljamalzim@gmail.com>
2022-05-22 13:03:55 +0600

    Solve problem #402

---
+++ 402. Remove K Digits/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def removeKdigits(self, num: str, k: int) -> str:       
+        stack = []
+        
+        for n in num:
+            while k and stack and stack[-1] > n:
+                stack.pop()
+                k -= 1
+
+            if stack or n != '0':
+                stack.append(n)
+
+        if k: stack = stack[:-k]
+
+        return ''.join(stack) if stack else '0'
