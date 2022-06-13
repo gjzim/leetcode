eaa533f73012488a67156893ebe94b75f7bd5177
Gul Jamal Zim <guljamalzim@gmail.com>
2022-06-13 20:43:41 +0600

    Solve problem #1283

---
+++ 1283. Find the Smallest Divisor Given a Threshold/solution.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def smallestDivisor(self, nums: List[int], threshold: int) -> int:
+        def valid(divisor):            
+            return sum((num - 1) // divisor + 1 for num in nums) <= threshold
+        
+        left, right = 1, max(nums)
+        while left < right:
+            mid = left + (right - left) // 2
+            
+            if valid(mid):
+                right = mid
+            else:
+                left = mid + 1
+                
+        return left
